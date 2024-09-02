9651d24d1ca8db217bc8319e4331433e9da6d2c7
SONGHEE <pasong0511@gmail.com>
2024-09-02 23:12:52 +0900

    feat : reset css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+
+import { Reset } from "styled-reset";
+
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import routes from "./routes";
 
