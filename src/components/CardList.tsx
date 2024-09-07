94c9fcc5d7e26cc2550363f191c542671e8b84f5
SONGHEE <pasong0511@gmail.com>
2024-09-07 23:19:05 +0900

    feat : CardList 컴포넌트로 이미지와 타이틀이 포함된 목록 보여주는 기능까지 구현

---
+++ src/components/CardList.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+interface CardListProps {
+    filteredItems: any[];
+}
+
+function CardList({ filteredItems }: CardListProps) {
+    return (
+        <ul className="card_list">
+            {filteredItems.map((item) => (
+                <li key={item.id}>
+                    <CardItem item={item} />
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+function CardItem({ item }: any) {
+    return (
+        <div className="card_item">
+            <Link to={`/portfolio/${item.id}`}>
+                <img
+                    src={item.imgSrc}
+                    alt={item.title}
+                    className="card_image"
+                />
+                <div className="card_overlay">
+                    <h3>{item.title}</h3>
+                </div>
+            </Link>
+        </div>
+    );
+}
+
+export default CardList;
